2375ad0be90b409fba431ac5e7047a4596718533
TEH YI HEN <tehyihen1994@gmail.com>
2026-01-15 09:02:50 +0800

    fix the deployment issue

--- backend/server.js
+++ backend/server.js
@@ -70,10 +70,10 @@ app.use("/api/products", productRoutes);
 
 if(process.env.NODE_ENV === "production"){
     
-    app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
     app.get(/.*/, (_,res) => {
-        res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"));
+        res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
     })
 }
 
